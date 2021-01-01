5903022b55e0c12bb183d3322d2847325bcfeba8
johannescarter <johannescarter@gmx.de>
2021-01-01 23:36:27 +0100

    Update admin_functions.php

--- admin/admin_functions.php
+++ admin/admin_functions.php
@@ -21,7 +21,45 @@ function jcs_cucj_activate() {
     $table_name = $wpdb->prefix . 'jcs_cucj_css_sheets';
 
     $sql = "CREATE TABLE $table_name (
-        id int NOT NULL AUTO_INCREMENT,
+        id int(255) NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    // create db table for css entries
+    $table_name = $wpdb->prefix . 'jcs_cucj_css_entries';
+
+    $sql = "CREATE TABLE $table_name (
+        id int(255) NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    // create db table for js files
+    $table_name = $wpdb->prefix . 'jcs_cucj_js_files';
+
+    $sql = "CREATE TABLE $table_name (
+        id int(255) NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    /*
+    global $wpdb;
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    // create db table for css sheets
+    $table_name = $wpdb->prefix . 'jcs_cucj_css_sheets';
+
+    $sql = "CREATE TABLE $table_name (
+        id int(255) NOT NULL AUTO_INCREMENT,
         name varchar(1023) NOT NULL,
         description varchar(8191),
         media_query varchar(8191),
@@ -42,7 +80,7 @@ function jcs_cucj_activate() {
         PRIMARY KEY (id)
     ) $charset_collate;";
 
-    //require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
 
     // create db table for js files
@@ -56,8 +94,9 @@ function jcs_cucj_activate() {
         PRIMARY KEY (id)
     ) $charset_collate;";
 
-    //require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
+    */
 
 }
 
