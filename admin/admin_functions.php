718407c0ad5a81398cc9a4dbe186aaa86e29f053
johannescarter <johannescarter@gmx.de>
2021-01-02 00:17:19 +0100

    Update admin_functions.php

--- admin/admin_functions.php
+++ admin/admin_functions.php
@@ -22,9 +22,10 @@ function jcs_cucj_activate() {
 
     $sql = "CREATE TABLE $table_name (
         id int(255) NOT NULL AUTO_INCREMENT,
-        name varchar(1023) NOT NULL,
+        name longtext NOT NULL,
         description longtext,
-        PRIMARY KEY  (id)
+        media_query vlongtext,
+        PRIMARY KEY (id)
     ) $charset_collate;";
 
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
@@ -37,7 +38,8 @@ function jcs_cucj_activate() {
         id int(255) NOT NULL AUTO_INCREMENT,
         selector longtext NOT NULL,
         description longtext,
-        PRIMARY KEY  (id)
+        custom_code longtext,
+        PRIMARY KEY (id)
     ) $charset_collate;";
 
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
@@ -50,59 +52,12 @@ function jcs_cucj_activate() {
         id int(255) NOT NULL AUTO_INCREMENT,
         name longtext NOT NULL,
         description longtext,
-        PRIMARY KEY  (id)
-    ) $charset_collate;";
-
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
-
-    /*
-    global $wpdb;
-
-    $charset_collate = $wpdb->get_charset_collate();
-
-    // create db table for css sheets
-    $table_name = $wpdb->prefix . 'jcs_cucj_css_sheets';
-
-    $sql = "CREATE TABLE $table_name (
-        id int(255) NOT NULL AUTO_INCREMENT,
-        name varchar(1023) NOT NULL,
-        description varchar(8191),
-        media_query varchar(8191),
+        custom_code longtext,
         PRIMARY KEY (id)
     ) $charset_collate;";
 
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
-
-    // create db table for css entries
-    $table_name = $wpdb->prefix . 'jcs_cucj_css_entries';
-
-    $sql = "CREATE TABLE $table_name (
-        id int(255) NOT NULL AUTO_INCREMENT,
-        selector varchar(8191) NOT NULL,
-        description varchar(8191),
-        custom_code varchar(65535),
-        PRIMARY KEY (id)
-    ) $charset_collate;";
-
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
-
-    // create db table for js files
-    $table_name = $wpdb->prefix . 'jcs_cucj_js_files';
-
-    $sql = "CREATE TABLE $table_name (
-        id int(255) NOT NULL AUTO_INCREMENT,
-        name varchar(1023) NOT NULL,
-        description varchar(8191),
-        custom_code varchar(65535),
-        PRIMARY KEY (id)
-    ) $charset_collate;";
-
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
-    */
 
 }
 
