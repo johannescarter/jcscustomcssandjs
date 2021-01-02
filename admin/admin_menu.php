efca6bce5347d806cc4069e223043168e8d692cd
johannescarter <johannescarter@gmx.de>
2021-01-02 19:46:23 +0100

    Update admin_menu.php

--- admin/admin_menu.php
+++ admin/admin_menu.php
@@ -30,6 +30,8 @@ function jcs_cucj_echo_button( $text, $name, $onclick = '', $href = '', $disable
 
 add_action('admin_menu', 'jcs_cucj_setup_menu');
 
+add_action('admin_enqueue_scripts', 'codemirror_enqueue_scripts');
+
 function codemirror_enqueue_scripts($hook) {
     $cm_settings['codeEditor'] = wp_enqueue_code_editor(array('type' => 'text/css'));
     wp_localize_script('jquery', 'cm_settings', $cm_settings);
@@ -164,7 +166,6 @@ function jcs_cucj_menu_page_css_files_callback( $submenu_page ) {
      * jcs_cucj_echo_button( 'new entry', 'button', "jcs_cucj_menu_get_view('css_files_new_entry', 'peter');" );
      * jcs_cucj_echo_button( 'edit entry', 'button', "jcs_cucj_menu_get_view('css_files_edit_entry', 'peter');" );
      */
-    add_action('admin_enqueue_scripts', 'codemirror_enqueue_scripts');
     ?>
         <div id="jcs_cucj_admin_menu_view_sockel">
             <?php cs_cucj_css_files_list_files_render_view(); ?>
