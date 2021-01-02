0705483868374213c05287d5efaf22945e9a439f
johannescarter <johannescarter@gmx.de>
2021-01-02 21:49:43 +0100

    Update admin_menu.php

--- admin/admin_menu.php
+++ admin/admin_menu.php
@@ -769,12 +769,12 @@ function cs_cucj_css_files_list_entries_render_view( $id ) {
                         ?>
                             <div class="jcs_cucj_list-item row">
                                 <div class="col-8">
-                                    <div class="row">
+                                    <div class="row flex-column">
                                         <p class="jcs_cucj_list-item-comment"><?= esc_html( $row->comment ); ?></p>
                                         <p class="jcs_cucj_list-item-css-code">
                                             <?= esc_html( $row->selector ); ?> {
                                                 <br />
-                                                    <div class="jcs_cucj_list-item-css-code_inner"><?= esc_html( $row->custom_code ); ?></div>
+                                                    <span class="jcs_cucj_list-item-css-code_inner"><?= esc_html( $row->custom_code ); ?></span>
                                                 <br />
                                             }
                                         </p>
